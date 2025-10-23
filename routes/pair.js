0478a06ed4e9202cc1a84dfeeb270eac10a72bbf
Kibetfx1 <kibetwycliffe093@gmail.com>
2025-10-23 16:00:31 +0300

    Update pair.js

--- routes/pair.js
+++ routes/pair.js
@@ -128,7 +128,7 @@ router.get('/', async (req, res) => {
                         while (sendAttempts < maxSendAttempts && !sessionSent) {
                             try {
                                 Sess = await Gifted.sendMessage(Gifted.user.id, {
-                                    text: 'Gifted~' + b64data
+                                    text: 'TEDDY-XMD~' + b64data
                                 });
                                 sessionSent = true;
                             } catch (sendError) {
