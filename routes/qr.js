2d9ec7f600ad1c7473fdd2c72f41e2348842d430
Maurice Gift <founder@giftedtech.co.ke>
2025-12-02 07:11:09 +0300

    Update: Fixed

--- routes/qr.js
+++ routes/qr.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
                             <!DOCTYPE html>
                             <html>
                             <head>
-                                <title>KEITH-MD | QR CODE</title>
+                                <title>GIFTED-MD | QR CODE</title>
                                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                                 <style>
                                     body {
@@ -162,7 +162,7 @@ router.get('/', async (req, res) => {
                             </head>
                             <body>
                                 <div class="container">
-                                    <h1>KEITH QR CODE</h1>
+                                    <h1>GIFTED QR CODE</h1>
                                     <div class="qr-container">
                                         <div class="qr-code pulse">
                                             <img src="${qrImage}" alt="QR Code"/>
@@ -189,7 +189,7 @@ router.get('/', async (req, res) => {
                 }
 
                 if (connection === "open") {
-                    await Gifted.groupAcceptInvite("KOvNtZbE3JC32oGAe6BQpp");
+                    await Gifted.groupAcceptInvite("GiD4BYjebncLvhr0J2SHAg");
  
                     await delay(10000);
 
